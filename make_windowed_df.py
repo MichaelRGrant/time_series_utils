f4d6b184a9e14488579ed0da7db89d5443a3fc43
Michael Grant <mike.mgrant@gmail.com>
2018-01-31 20:25:14 -0800

    added the response width functionality

--- make_windowed_df.py
+++ make_windowed_df.py
@@ -1,17 +1,26 @@
-def make_windowed(dataset, seq_length, horizon, feat_cols, resp_cols, region_col, split=0.2, predict_current=False):
+def make_windowed(dataset, seq_length, horizon, feat_cols, resp_cols, region_col, split=0.2, resp_width=0,
+                        predict_current=False):
     """
     This function takes data and creates a windowed dataframe to be used in time-series analysis. 
 
     dataset: [panda df] the raw dataframe that a time series windowed df will be created from
-    seq_length: [int] the sequence length, the amount of time to be used to perform the time-series analysis
+    seq_length: [int] the sequence length, the amount of time to be used to perform the 
+                time-series analysis
     horizon: [int] how far into the future you wish to predict
     feat_cols: [list] a list of column names that make up the feature space
     resp_cols: [list] a list of column names that make up the response
-    region_col: [str] the name of the column that different time-series will be created on, i.e. different regions that contain
+    region_col: [str] the name of the column that different time-series will be created on, i.e. 
+                different regions that contain
                 independent time-series.
-    split: [float] A percent split for the test set, i.e. 0.2 equals a 80/20 split for the train/test sets.
-    predict_current: [bool] horizon needs to be set to 1 for this to work. This will predict at the current time
-                    so if there is a sequence length of 2 instead of forecasting out the horizon length, the model
+    split: [float] A percent split for the test set, i.e. 0.2 equals a 80/20 split for the 
+                train/test sets.
+    resp_width: [int] If you want to predict out to a set distance you set the horizon to that 
+                    time point and this value to 0, however if you want to predict every value 
+                    between let's say now and some point in the future you set horizon to 1 and
+                    the resp_width to that point. The algorithm will then predict every time point.
+    predict_current: [bool] horizon needs to be set to 1 for this to work. This will predict at 
+                    the current time so if there is a sequence length of 2 instead of forecasting 
+                    out the horizon length, the model
                     will predict at the current time.
     """
 
@@ -21,24 +30,27 @@ def make_windowed(dataset, seq_length, horizon, feat_cols, resp_cols, region_col
         pass
 
     if (any(dataset[feat_cols + resp_cols].isnull().sum()) != 0):
-        raise ValueError(
-            'There is missing data in at least one of the columns supplied. Please impute this missing data as needed.')
+        raise ValueError('There is missing data in at least one of the columns supplied in keep_cols. \
+                         Please impute this missing data as needed.')
     else:
         pass
 
     # check to see if there are same features in both the response and the features list
     resp_and_feats = [var for var in feat_cols if var in resp_cols]
 
-    dense = len(resp_cols)
     regions = list(dataset[region_col].unique())
     big_dict = {}
 
     for i in range(len(regions)):
         big_dict.update({regions[i]: dataset[dataset[region_col] == regions[i]]})
     features = len(feat_cols)
-
-    train_X_all, test_X_all = np.empty((0, seq_length, features)), np.empty((0, seq_length, features))
-    train_y_all, test_y_all = np.empty((0, dense)), np.empty((0, dense))
+    response = len(resp_cols)
+    if resp_width == 0:
+        train_X_all, test_X_all = np.empty((0, seq_length, features)), np.empty((0, seq_length, features))
+        train_y_all, test_y_all = np.empty((0, response)), np.empty((0, response))
+    else:
+        train_X_all, test_X_all = np.empty((0, sequence_length, features)), np.empty((0, sequence_length, features))
+        train_y_all, test_y_all = np.empty((0, resp_width, response)), np.empty((0, resp_width, response))
 
     for region in regions:
         big_dict[region] = big_dict[region][resp_cols + feat_cols]
@@ -48,7 +60,6 @@ def make_windowed(dataset, seq_length, horizon, feat_cols, resp_cols, region_col
             pass
 
         length = len(big_dict[region])
-        print(length)
         test_length = int(length * split)  # 20% test set
 
         df_x = big_dict[region][feat_cols]
@@ -67,13 +78,21 @@ def make_windowed(dataset, seq_length, horizon, feat_cols, resp_cols, region_col
             z = 1
             q = 0
 
-        for i in range(train_length - seq_length - horizon):
-            train_X.append(ts_x[i:i + seq_length])
-            train_y.append(ts_y[i + seq_length + horizon - z])
+        if (resp_width != 0):
+            for i in range(train_length - sequence_length - horizon - resp_width):
+                train_X.append(ts_x[i:i + sequence_length])
+                train_y.append(ts_y[i + sequence_length + horizon - z:i + sequence_length + horizon - z + resp_width])
+            for i in range(-test_length, -resp_width):
+                test_X.append(ts_x[i - sequence_length - horizon + 1:i - horizon + 1])
+                test_y.append(ts_y[i - q:i - q + (resp_width)])
 
-        for i in range(-test_length, 0):
-            test_X.append(ts_x[i - seq_length - horizon + 1:i - horizon + 1])
-            test_y.append(ts_y[i - q])
+        else:
+            for i in range(train_length - seq_length - horizon):
+                train_X.append(ts_x[i:i + seq_length])
+                train_y.append(ts_y[i + seq_length + horizon - z])
+            for i in range(-test_length, 0):
+                test_X.append(ts_x[i - seq_length - horizon + 1:i - horizon + 1])
+                test_y.append(ts_y[i - q])
 
         train_X, train_y = np.array(train_X), np.array(train_y)
         test_X, test_y = np.array(test_X), np.array(test_y)
@@ -99,6 +118,14 @@ def make_windowed(dataset, seq_length, horizon, feat_cols, resp_cols, region_col
     test_y = (test_y_all - mean_y) / std_y
     train_y = train_y.astype('float32')
     test_y = test_y.astype('float32')
+
+    if resp_width != 0:
+        std_y = std_y.ravel()
+        mean_y = mean_y.ravel()
+        train_y = train_y.reshape(train_y.shape[0], train_y.shape[1] * response)
+        test_y = test_y.reshape(test_y.shape[0], test_y.shape[1] * response)
+    else:
+        pass
 
     print('train_X shape:', np.shape(train_X))
     print('train_y shape:', np.shape(train_y))
